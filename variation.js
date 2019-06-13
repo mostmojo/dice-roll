bca075f0fc980bad895877a1bd37a64b0fcd6398
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-13 09:19:40 +0100

    Add comments

--- variation.js
+++ variation.js
@@ -2,7 +2,7 @@
 
 Change the game:
 
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn.
+1. A player loses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn.
 (Hint: Always save the previous dice roll in a separate variable)
 2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
@@ -14,6 +14,7 @@ init();
 
 var lastDice; // Needs to be in global scope because data gets lost in function scope.
 
+// --- DICE ROLL --- //
 document.querySelector(".btn-roll").addEventListener('click', function() {
     if(gamePlaying) {
         // 1. Random number
@@ -41,6 +42,7 @@ document.querySelector(".btn-roll").addEventListener('click', function() {
     }
 });
 
+// --- DICE HOLD --- //
 document.querySelector(".btn-hold").addEventListener("click", function() {
     if(gamePlaying) {
         // Add CURRENT score to GLOBAL score
@@ -61,6 +63,7 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     }
 })
 
+// --- NEXT PLAYER --- //
 function nextPlayer() {
     // Next player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -79,6 +82,8 @@ function nextPlayer() {
 // we don't add init() as we only want function to be called upon btn-new button click. So, pass it as a param.
 document.querySelector(".btn-new").addEventListener("click", init);
 
+
+// --- START GAME --- //
 function init() {
     scores = [0, 0];
     activePlayer = 0;
